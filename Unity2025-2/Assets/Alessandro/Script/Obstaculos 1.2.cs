80ff2f86fb04309d65bfd6b54d0558dfa1a2791a
Caiiru <54562856+Caiiru@users.noreply.github.com>
2025-11-19 17:11:15 -0300

    Alessandro

---
+++ Unity2025-2/Assets/Alessandro/Script/Obstaculos 1.2.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class MoveObstaculo2 : MonoBehaviour
+{
+    public float distancia = 4.5f;   // amplitude do movimento
+    public float velocidade = 2f;  // velocidade da oscilação
+    public bool inverterInicio = false; // se true, começa do lado oposto
+
+    private Vector3 posicaoInicial;
+    private float faseInicial;
+
+    void Start()
+    {
+        posicaoInicial = transform.position;
+
+        // Se inverterInicio for true, começa com 180° de diferença (π radianos)
+        faseInicial = inverterInicio ? Mathf.PI : 0f;
+    }
+
+    void Update()
+    {
+        float deslocamento = Mathf.Sin(Time.time * velocidade + faseInicial) * distancia;
+        transform.position = posicaoInicial + Vector3.right * deslocamento;
+    }
+}
